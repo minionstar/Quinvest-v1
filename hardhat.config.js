cc0da0a4ced7da02d415187aff3459bd0ab5aead
minionstar <louislee214@gmail.com>
2023-07-28 02:49:13 -0400

    finish treasury contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,3 @@
-require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 /** @type import('hardhat/config').HardhatUserConfig */
